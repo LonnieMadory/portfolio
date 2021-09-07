9af1dbb0b252a52662e17e3cb46bbc4c1333f708
Lonnie Madory <Lonnie.Madory@sckans.edu>
2021-09-07 15:14:13 -0500

    edit

---
+++ docs/precache-manifest.ec3bc2a6564ec0dbc32c08114ed13198.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876d7dcafbb75a31b4666c9c78bd6fce",
+    "url": "/LonnieMadory.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "58178f42ee4c655b3bf8",
+    "url": "/LonnieMadory.github.io/portfolio/static/css/main.3c4fcfb7.chunk.css"
+  },
+  {
+    "revision": "6f96f6c733c26e6c8fcf",
+    "url": "/LonnieMadory.github.io/portfolio/static/js/2.fd3a679b.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/LonnieMadory.github.io/portfolio/static/js/2.fd3a679b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58178f42ee4c655b3bf8",
+    "url": "/LonnieMadory.github.io/portfolio/static/js/main.04485833.chunk.js"
+  },
+  {
+    "revision": "ddef14917f317c1e607a",
+    "url": "/LonnieMadory.github.io/portfolio/static/js/runtime-main.199801f5.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/LonnieMadory.github.io/portfolio/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
